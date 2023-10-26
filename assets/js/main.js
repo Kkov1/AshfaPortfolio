954c4df6fc648a760a17368ad0daf7785f7204f4
Kkovi1 <muhammadashfa2006@gmail.com>
2023-10-26 15:33:51 +0700

    Update Website

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,7 +49,29 @@ const skillsContent = document.getElementsByClassName('skills__content'),
         el.addEventListener('click', toggleSkills);
     });    
 
+/*======================== Mail Message ============================*/ 
+function sendMail() {
+    var params = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        subject: document.getElementById('subject').value,
+        message: document.getElementById('message').value 
+    };
 
+    const serviceId = "service_tbgjwdk"; 
+    const templateId = "template_cjnai2y"; 
+
+    emailjs
+        .send(serviceId, templateId, params)
+        .then((res) => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("subject").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+        })
+        .catch((err) => console.log(err));
+}
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
 function scrollHeader(){
     const nav = document.getElementById('header')
